b7a66237d200c90a9f9fac9d8eed540ab61c1171
KAZUHIRO YAMAMOTO <ykz.91654027@gmail.com>
2025-12-17 18:13:48 +0900

    feat:Link Footer コンポーネントを作成

---
+++ src/componets/Footer.tsx
@@ -0,0 +1,10 @@
+
+export function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white py-4">
+      <div className="container mx-auto text-center">
+        <p>&copy; {new Date().getFullYear()} Your Company. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
