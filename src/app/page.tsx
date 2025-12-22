cb39dc884bb030fbf395b4364bccbe7cda483d7b
KAZUHIRO YAMAMOTO <ykz.91654027@gmail.com>
2025-12-23 07:19:02 +0900

    feat:useEffectの処理を追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
 "use client";
 
 import { Main } from "@/components/Main";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 
 export default function Home() {
   const handleClick = useCallback((e: React.MouseEvent<HTMLAnchorElement>) => {
     console.log((e.target as HTMLAnchorElement).href);
     e.preventDefault();
+  }, []);
+
+  useEffect(() => {
+    console.log("マウント時");
+    document.body.style.backgroundColor = "lightblue";
+    return () => {
+      console.log("アンマウント時");
+      document.body.style.backgroundColor = "";
+    };
   }, []);
 
   return (
