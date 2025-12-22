9075c62e5153226dc5c29ef7bce81725ba1ddbbe
KAZUHIRO YAMAMOTO <ykz.91654027@gmail.com>
2025-12-22 13:55:05 +0900

    test:クリックイベントを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+"use client";
+
 import { Main } from "@/components/Main";
+import { useCallback } from "react";
 
 export default function Home() {
+  const handleClick = useCallback((e: React.MouseEvent<HTMLAnchorElement>) => {
+    console.log((e.target as HTMLAnchorElement).href);
+    e.preventDefault();
+  }, []);
+
   return (
-    <Main page="Index" />
+    <>
+      <a href="/about" onClick={handleClick}>
+        ボタン
+      </a>
+      <Main page="Index" />
+    </>
   );
 }
