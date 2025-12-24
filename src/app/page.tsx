a686a01ba71a7e60f015f13386295c3db22d3388
KAZUHIRO YAMAMOTO <ykz.91654027@gmail.com>
2025-12-25 07:48:00 +0900

    feat:useCallback useEffect の第2引数について確認

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,20 @@
 "use client";
 
 import { Main } from "@/components/Main";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import styles from "@/app/page.module.css";
 
 export default function Home() {
-
   const [count, setCount] = useState(1);
 
-
-  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    setCount (count => count + 1);
-    setCount (count => count + 1);
-  }
+  const handleClick = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement>) => {
+      if (count < 10) {
+        setCount((count) => count + 1);
+      }
+    },
+    [count]
+  );
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
@@ -21,11 +23,12 @@ export default function Home() {
     };
   }, []);
 
-
   return (
     <>
-    <h1 className={styles.heading}>{count}</h1>
-      <button className={styles.button} onClick={handleClick}>ボタン</button>
+      <h1 className={styles.heading}>{count}</h1>
+      <button className={styles.button} onClick={handleClick}>
+        ボタン
+      </button>
       <Main page="Index" />
     </>
   );
