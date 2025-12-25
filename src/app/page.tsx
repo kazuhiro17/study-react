53b7c332baba3309285b249d54562b871c71cdde
KAZUHIRO YAMAMOTO <ykz.91654027@gmail.com>
2025-12-25 11:42:15 +0900

    feat:useStateで文字列と真偽値について確認

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ import styles from "@/app/page.module.css";
 
 export default function Home() {
   const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
 
   const handleClick = useCallback(
     (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -16,6 +18,10 @@ export default function Home() {
     [count]
   );
 
+  const handleDisplay = useCallback(() => {
+    setIsShow((isShow) => !isShow);
+  }, []);
+
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
     return () => {
@@ -23,12 +29,29 @@ export default function Home() {
     };
   }, []);
 
+  const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value.trim().length > 5) {
+      alert("5文字以内にしてください");
+      return;
+    }
+    setText(e.target.value.trim());
+  }, []);
+
   return (
     <>
-      <h1 className={styles.heading}>{count}</h1>
+      {isShow ? <h1 className={styles.heading}>{count}</h1> : null}
       <button className={styles.button} onClick={handleClick}>
         ボタン
       </button>
+      <button className={styles.button} onClick={handleDisplay}>
+        {isShow ? "非表示" : "表示"}
+      </button>
+      <input
+        type="text"
+        className={styles.input}
+        value={text}
+        onChange={handleChange}
+      />
       <Main page="Index" />
     </>
   );
