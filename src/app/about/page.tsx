a1293dd1158b6105ffa3a6ed04c600abf3791ba0
KAZUHIRO YAMAMOTO <ykz.91654027@gmail.com>
2025-12-27 13:49:48 +0900

    refactor:Custom Hooksを作成

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,46 @@
+"use client";
+
 import { Main } from "@/components/Main";
+import { useCounter } from "@/hooks/useCounter";
+import { useInputArray } from "@/hooks/useInputArray";
+import { useBgLightblue } from "@/hooks/useBgLightblue";
+import styles from "@/app/page.module.css";
 
 export default function About() {
+  const { count, isShow, handleClick, handleDisplay } = useCounter();
+  const { text, array, handleChange, handleAdd } = useInputArray();
+  useBgLightblue();
+
   return (
-    <Main page="About" />
+    <>
+      {isShow ? <h1 className={styles.heading}>{count}</h1> : null}
+      <button className={styles.button} onClick={handleClick}>
+        ボタン
+      </button>
+      <button className={styles.button} onClick={handleDisplay}>
+        {isShow ? "非表示" : "表示"}
+      </button>
+
+      <input
+        name="text"
+        type="text"
+        className={styles.input}
+        value={text}
+        onChange={handleChange}
+      />
+      <button className={styles.button} onClick={handleAdd}>
+        追加
+      </button>
+      <ul>
+        {array.map((item) => {
+          return (
+            <li className={styles.item} key={item}>
+              {item}
+            </li>
+          );
+        })}
+      </ul>
+      <Main page="About" />
+    </>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,17 @@
 "use client";
 
 import { Main } from "@/components/Main";
-import { useCallback, useEffect, useState } from "react";
+import { useCounter } from "@/hooks/useCounter";
+import { useInputArray } from "@/hooks/useInputArray";
+import { useBgLightblue } from "@/hooks/useBgLightblue";
 import styles from "@/app/page.module.css";
 
+
+
 export default function Home() {
-  const [count, setCount] = useState(1);
-  const [text, setText] = useState("");
-  const [isShow, setIsShow] = useState(true);
-  const [array, setArray] = useState<string[]>([]);
-
-  const handleClick = useCallback(() => {
-    if (count < 10) {
-      setCount((prevCount) => prevCount + 1);
-    }
-  }, [count]);
-
-  const handleDisplay = useCallback(() => {
-    setIsShow((prevIsShow) => !prevIsShow);
-  }, []);
-
-  const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.value.trim().length > 5) {
-      alert("5文字以内にしてください");
-      return;
-    }
-    setText(e.target.value.trim());
-  }, []);
-
-  const handleAdd = useCallback(() => {
-    setArray((prevArray) => {
-      if (prevArray.some((item) => item === text)) {
-        alert("同じ要素が既に存在しています");
-        return prevArray;
-      }
-      return [...prevArray, text];
-    });
-  }, [text]);
-
-  useEffect(() => {
-    document.body.style.backgroundColor = "lightblue";
-    return () => {
-      document.body.style.backgroundColor = "";
-    };
-  }, []);
+  const { count, isShow, handleClick, handleDisplay } = useCounter();
+  const { text, array, handleChange, handleAdd } = useInputArray();
+  useBgLightblue();
 
   return (
     <>
@@ -54,6 +22,7 @@ export default function Home() {
       <button className={styles.button} onClick={handleDisplay}>
         {isShow ? "非表示" : "表示"}
       </button>
+
       <input
         name="text"
         type="text"
@@ -61,10 +30,16 @@ export default function Home() {
         value={text}
         onChange={handleChange}
       />
-      <button className={styles.button} onClick={handleAdd}>追加</button>
+      <button className={styles.button} onClick={handleAdd}>
+        追加
+      </button>
       <ul>
         {array.map((item) => {
-          return <li className={styles.item} key={item}>{item}</li>;
+          return (
+            <li className={styles.item} key={item}>
+              {item}
+            </li>
+          );
         })}
       </ul>
       <Main page="Index" />
